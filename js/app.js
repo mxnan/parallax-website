41aca6a8cbbdf45af98f4563ee76875c28b0c987
mxnan <128995981+mxnan@users.noreply.github.com>
2023-09-25 16:43:28 +0530

    page done

---
+++ js/app.js
@@ -0,0 +1,42 @@
+const parallax_el = document.querySelectorAll(".parallax");
+
+let xValue = 0, yValue = 0;
+
+let rotateDegree = 0;
+
+function update(cursorPosition) {
+  parallax_el.forEach((el) => {
+    let speedx = el.dataset.speedx;
+    let speedy = el.dataset.speedy;
+    let speedz = el.dataset.speedz;
+    let rotateSpeed = el.dataset.rotation;
+    
+    
+    let isinleft = parseFloat(getComputedStyle(el).left) < window.innerwidth / 2 ? 1 : -1;
+    let zValue = (cursorPosition - parseFloat(getComputedStyle(el).left)) * isinleft * 0.01 ;
+
+    el.style.transform = 
+    `
+    perspective(2300px) translateZ(${zValue * speedz}px)
+    rotateY(${rotateDegree * rotateSpeed }deg)
+    translateX(calc(-50% + ${ -xValue * speedx }px)) 
+    translateY(calc(-50% + ${  yValue * speedy   }px)) 
+    
+    `;
+   });
+
+}
+
+update(0);
+
+window.addEventListener("mousemove", (e) => {
+   xValue = e.clientX - window.innerWidth / 2;
+   yValue = e.clientY - window.innerHeight / 2;
+
+   rotateDegree = (xValue / (window.innerWidth / 2)) * 20;
+
+   //console.log(xValue,yValue);
+
+   update(e.clientX);
+ 
+});
